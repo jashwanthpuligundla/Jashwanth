af524d8b0df82e298e4d7684a9f6737f6f64226b
jashwanthpuligundla <45967934+jashwanthpuligundla@users.noreply.github.com>
2019-02-01 18:02:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 app.use(express.static(path.join(__dirname,'public')));
 app.get('/', function(req, res){
     
-    res.sendFile('hi.html');
+    res.sendFile('index.html');
 });
 
 var port = process.env.PORT || 8080;
